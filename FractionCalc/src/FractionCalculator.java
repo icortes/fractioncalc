37954bd2e0b53095d3a1e56666d3b5cca0b02a03
icepac12 <icepac12@yahoo.com>
2013-11-04 18:18:12 -0800

    more changes

--- FractionCalc/src/FractionCalculator.java
+++ FractionCalc/src/FractionCalculator.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class FractionCalculator {
-	public static int[] firstHalf = new int[2];
-	public static int[] secondHalf = new int[2];
-	public static int[] answer = new int[2];
 
 	public static void main(String[] args) {
+		int[] firstHalf = new int[2];
+		int[] secondHalf = new int[2];
+		int[] answer = new int[2];
+		String operator = null;
 		String in = inputFromUser();
 		while (!in.equalsIgnoreCase("quit")) {
-			separate(in);
-
+			operator = findOperator(in.trim());
+			separate(in, firstHalf, secondHalf);
+			
 			in = inputFromUser();
 		}
 	}
@@ -17,6 +19,12 @@ public class FractionCalculator {
 	public static String inputFromUser() {
 		return new Scanner(System.in).nextLine();
 
+	}
+
+	public static void separate(String equ, int[] firstHalf, int[] secondHalf) {
+		String operator = findOperator(equ);
+		setToArray(equ.substring(0, equ.indexOf(operator)), firstHalf);
+		setToArray(equ.substring(equ.indexOf(operator) + 1), secondHalf);
 	}
 
 	public static int findUnderscore(String in) {
@@ -60,15 +68,6 @@ public class FractionCalculator {
 		return arr;
 	}
 
-	public static void separate(String equ) {
-		String operator = findOperator(equ);
-		setFirstHalf(equ.substring(0, equ.indexOf(operator)));
-		setSecondHalf(equ.substring(equ.indexOf(operator) + 1));
-		if (operator == "+") {
-			
-		}
-	}
-
 	public static String findOperator(String in) {
 		String operator = null;
 		if (in.contains("+"))
@@ -82,17 +81,11 @@ public class FractionCalculator {
 		return operator;
 	}
 
-	public static void setFirstHalf(String frac) {
-		firstHalf = setVars(frac);
-		for (int element : firstHalf)
+	public static void setToArray(String frac, int[] arr) {
+		arr = setVars(frac);
+		for (int element : arr)
 			System.out.print(element + " ");
 		System.out.println();
-	}
-
-	public static void setSecondHalf(String frac) {
-		secondHalf = setVars(frac);
-		for (int element : secondHalf)
-			System.out.print(element + " ");
 	}
 
 	public static int getLCM(int a, int b) {
